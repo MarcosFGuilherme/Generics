296acdd8b4b8262e2a07860e15ce7c684b8bea16
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-09 14:49:27 -0300

    Start project - Make input screen.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,22 @@
 package application;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("How many values? ");
+		int values = sc.nextInt();
+		String numbers = "[";
+		
+		for (int n=1; n<=values;n++) {
+			System.out.print("Input number #" + n + ": ");
+			int number = sc.nextInt();
+			numbers += number  + ",";
+		}
+		System.out.println();
+		System.out.println(numbers.substring(0, numbers.length()-1 ) +"]");
+		sc.close();
+	}
 }
